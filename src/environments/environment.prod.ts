ae99765107c5ee4fe569a7f486069de4de9538ba
Paritosh Patil <paritosh.patil18@vit.edu>
2024-08-04 04:15:12 +0530

    shhhh

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  spotify_client_id: 'b1f67cbbf69f448cb0d0d0ec4f7a2d59',
+  spotify_client_secret: '4819ead624034032ba29df0f3588adff'
 };
